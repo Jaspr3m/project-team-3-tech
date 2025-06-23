1a316f74f5b21ea134cbed7f3dc1e4910f33a269
ivyv0 <145538685+ivyv0@users.noreply.github.com>
2025-06-23 14:52:05 +0200

    NEWNWENWN

--- server.js
+++ server.js
@@ -236,7 +236,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({
   storage,
-  limits: { fileSize: 2 * 1024 * 1024 }, // 2 MB
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5 MB
   fileFilter: (_, file, cb) =>
     file.mimetype.startsWith("image/")
       ? cb(null, true)
